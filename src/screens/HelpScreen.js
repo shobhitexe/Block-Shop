abe44e34b8ef872bebd2cdf28f40e3a469a38418
Shobhit Mirjankar <shobhitmir@gmail.com>
2023-05-27 11:03:17 +0530

    Updated HelpScreen.js

--- src/screens/HelpScreen.js
+++ src/screens/HelpScreen.js
@@ -6,8 +6,8 @@ export default function HelpScreen() {
         <h1>Steps to Connect Metamask with our Dapp </h1>
         <p>1. Download metamask extension for your specific browser</p>
         <p>2. Create a wallet or import an existing wallet using metamask</p>
-        <p>3. Select the Rinkeby Test Network using Metamask 
-            as the Dapp is currently deployed on Rinkeby Test Network</p>
+        <p>3. Select the Sepolia Test Network using Metamask 
+            as the Dapp is currently deployed on Sepolia Test Network</p>
         <p>4. Visit the homepage of our website and you will be prompted to establish 
             connection with our Dapp using one of your metamask accounts
         </p>
@@ -18,7 +18,7 @@ export default function HelpScreen() {
         <p>1. Currently the Dapp has been deployed on test net so users can experience
             Block Shop by paying for products using test credit cards.</p>
         <p>2. Fake ether
-            can be used to add reviews. <a href="https://faucets.chain.link/rinkeby">Click here</a> to get some ether for testing purposes.</p> 
+            can be used to add reviews. <a href="https://sepoliafaucet.com/">Click here</a> to get some ether for testing purposes.</p> 
         <p>3. Please email us your queries or issues encountered 
             at <a href = "mailto: blockshoptest@gmail.com">blockshoptest@gmail.com</a> so that 
             we can rectify the issues faced. </p>
